0a0304bdb492533049d72fa985bf36c0126fab73
Alireza Fakharzadeh <51977254+s3rver@users.noreply.github.com>
2023-09-08 20:40:07 +0330

    update service -> purchases

--- purchases.js
+++ purchases.js
@@ -1,1 +1,1 @@
-[{"name":"dasdas"},{"name":"dsadas"},{"name":"34234"},{"name":"dsfsd"},{"name":"fsdfsd333"},{"name":"111111111111111111111"}]
+[{"name":"dasdas"},{"name":"dsadas"},{"name":"34234"},{"name":"dsfsd"},{"name":"fsdfsd333"},{"name":"111111111111111111111"},{"name":"2222222222222222"}]
